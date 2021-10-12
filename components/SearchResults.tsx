cf8adb1afcd17175d1b36c25e57c0b706f2bfbc3
Fillipi Pereira <pfillipi@gmx.de>
2021-10-12 04:01:11 +0200

    Peng

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -25,6 +25,9 @@ export function SearchResults({ results }: SearchResultsProps) {
 */
 
 /**
+ * Apply memo in the following situations:
  * 1. Pure Functional Components
  * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
  */
