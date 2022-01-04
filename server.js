948f2511afc30cfc7fb456ed174783d76124a22e
Fillipi Pereira <pfillipi@gmx.de>
2022-01-04 23:49:14 +0100

    Mer en -

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ module.exports = () => {
   for (let i = 0; i < 1000; i++) {
     data.products.push({
       id: i + 1,
-      price: 080,
-      title: `Skjorte ${i + 1}`,
+      price: 80,
+      title: `T-Skjorte ${i + 1}`,
     });
   }
 
