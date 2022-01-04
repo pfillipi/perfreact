2b8ab571f4aa3536af0e77ffdec4309aa0d3fc06
Fillipi Pereira <pfillipi@gmx.de>
2022-01-04 20:34:01 +0100

    Search Results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
 
 export default function Home() {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
 
-  function handleSearch() {}
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
 
   return (
     <div>
@@ -17,6 +30,8 @@ export default function Home() {
         />
         <button type="submit">Søk</button>
       </form>
+
+      <SearchResults results={results} />
     </div>
   );
 }
