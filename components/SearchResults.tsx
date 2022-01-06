50d973608ed1d562ed5da58589d6b3e64a52c038
Fillipi Pereira <pfillipi@gmx.de>
2022-01-06 03:28:56 +0100

    Kommentarer

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -16,3 +16,10 @@ export function SearchResults({ results }: SearchResultsProps) {
     </div>
   );
 }
+
+/** 
+  * 1. Lag en ny versjon av komponenten;
+  * 2. Sammenlign med den fremre versjonen;
+  * 3. Hvis det er endringer, oppdater dem.
+
+*/
